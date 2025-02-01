76a291b631c0147ab036c201711b0a81cda0fe56
amandeep-boot <aman509karan@gmail.com>
2025-02-01 23:24:48 +0530

    limit the bullets

--- settings.py
+++ settings.py
@@ -15,7 +15,7 @@ class Settings:
         self.bullet_width = 3
         self.bullet_height=15.0
         self.bullet_color = (60,60,60)
-        self.bullet_allowed = 500
+        self.bullet_allowed = 5
 
         # Alien settings
         self.alien_speed=1.0
